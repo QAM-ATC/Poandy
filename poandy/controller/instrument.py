22c19bb6dd70aacec8316f2ae588b1a12d3d57d0
callistachang <changcallista@gmail.com>
2020-10-10 20:53:29 +0800

    Fix Pytest/Black

--- poandy/controller/instrument.py
+++ poandy/controller/instrument.py
@@ -4,6 +4,7 @@ from poandy.controller.base import Controller
 import pandas as pd
 from dateutil.parser import isoparse
 
+
 class InstrumentController(Controller):
     @classmethod
     def get_candles(cls, instrument_name, candle_params={}, df=True):
